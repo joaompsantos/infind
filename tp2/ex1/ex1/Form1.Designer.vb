e1f7ca6763b3ee7cd0fc3ed32afeb82f5c2a1673
João Santos <joao.mp.santos@ua.pt>
2019-09-27 01:41:34 +0100

    Tp2: ex1 Final

---
+++ tp2/ex1/ex1/Form1.Designer.vb
@@ -0,0 +1,166 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblOper1 = New System.Windows.Forms.Label()
+        Me.lblOper2 = New System.Windows.Forms.Label()
+        Me.lblRes = New System.Windows.Forms.Label()
+        Me.txtOper1 = New System.Windows.Forms.TextBox()
+        Me.txtOper2 = New System.Windows.Forms.TextBox()
+        Me.txtRes = New System.Windows.Forms.TextBox()
+        Me.btnSum = New System.Windows.Forms.Button()
+        Me.btnSub = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.btnDiv = New System.Windows.Forms.Button()
+        Me.btnTimes = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lblOper1
+        '
+        Me.lblOper1.AutoSize = True
+        Me.lblOper1.Location = New System.Drawing.Point(65, 53)
+        Me.lblOper1.Name = "lblOper1"
+        Me.lblOper1.Size = New System.Drawing.Size(57, 20)
+        Me.lblOper1.TabIndex = 0
+        Me.lblOper1.Text = "Label1"
+        Me.lblOper1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblOper2
+        '
+        Me.lblOper2.AutoSize = True
+        Me.lblOper2.Location = New System.Drawing.Point(241, 53)
+        Me.lblOper2.Name = "lblOper2"
+        Me.lblOper2.Size = New System.Drawing.Size(57, 20)
+        Me.lblOper2.TabIndex = 1
+        Me.lblOper2.Text = "Label2"
+        '
+        'lblRes
+        '
+        Me.lblRes.AutoSize = True
+        Me.lblRes.Location = New System.Drawing.Point(425, 53)
+        Me.lblRes.Name = "lblRes"
+        Me.lblRes.Size = New System.Drawing.Size(57, 20)
+        Me.lblRes.TabIndex = 2
+        Me.lblRes.Text = "Label3"
+        '
+        'txtOper1
+        '
+        Me.txtOper1.Location = New System.Drawing.Point(69, 76)
+        Me.txtOper1.Name = "txtOper1"
+        Me.txtOper1.Size = New System.Drawing.Size(100, 26)
+        Me.txtOper1.TabIndex = 3
+        '
+        'txtOper2
+        '
+        Me.txtOper2.Location = New System.Drawing.Point(245, 76)
+        Me.txtOper2.Name = "txtOper2"
+        Me.txtOper2.Size = New System.Drawing.Size(100, 26)
+        Me.txtOper2.TabIndex = 4
+        '
+        'txtRes
+        '
+        Me.txtRes.Location = New System.Drawing.Point(429, 76)
+        Me.txtRes.Name = "txtRes"
+        Me.txtRes.Size = New System.Drawing.Size(100, 26)
+        Me.txtRes.TabIndex = 5
+        '
+        'btnSum
+        '
+        Me.btnSum.Location = New System.Drawing.Point(69, 141)
+        Me.btnSum.Name = "btnSum"
+        Me.btnSum.Size = New System.Drawing.Size(100, 43)
+        Me.btnSum.TabIndex = 6
+        Me.btnSum.Text = "Button1"
+        Me.btnSum.UseVisualStyleBackColor = True
+        '
+        'btnSub
+        '
+        Me.btnSub.Location = New System.Drawing.Point(194, 141)
+        Me.btnSub.Name = "btnSub"
+        Me.btnSub.Size = New System.Drawing.Size(104, 43)
+        Me.btnSub.TabIndex = 7
+        Me.btnSub.Text = "Button2"
+        Me.btnSub.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(331, 141)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(135, 114)
+        Me.btnExit.TabIndex = 8
+        Me.btnExit.Text = "Button3"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'btnDiv
+        '
+        Me.btnDiv.Location = New System.Drawing.Point(194, 233)
+        Me.btnDiv.Name = "btnDiv"
+        Me.btnDiv.Size = New System.Drawing.Size(104, 39)
+        Me.btnDiv.TabIndex = 9
+        Me.btnDiv.Text = "Button4"
+        Me.btnDiv.UseVisualStyleBackColor = True
+        '
+        'btnTimes
+        '
+        Me.btnTimes.Location = New System.Drawing.Point(69, 233)
+        Me.btnTimes.Name = "btnTimes"
+        Me.btnTimes.Size = New System.Drawing.Size(100, 39)
+        Me.btnTimes.TabIndex = 10
+        Me.btnTimes.Text = "Button5"
+        Me.btnTimes.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnTimes)
+        Me.Controls.Add(Me.btnDiv)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnSub)
+        Me.Controls.Add(Me.btnSum)
+        Me.Controls.Add(Me.txtRes)
+        Me.Controls.Add(Me.txtOper2)
+        Me.Controls.Add(Me.txtOper1)
+        Me.Controls.Add(Me.lblRes)
+        Me.Controls.Add(Me.lblOper2)
+        Me.Controls.Add(Me.lblOper1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblOper1 As Label
+    Friend WithEvents lblOper2 As Label
+    Friend WithEvents lblRes As Label
+    Friend WithEvents txtOper1 As TextBox
+    Friend WithEvents txtOper2 As TextBox
+    Friend WithEvents txtRes As TextBox
+    Friend WithEvents btnSum As Button
+    Friend WithEvents btnSub As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents btnDiv As Button
+    Friend WithEvents btnTimes As Button
+End Class
---
+++ tp2/ex1/ex1/Form1.vb
@@ -0,0 +1,131 @@
+﻿'
+' João Santos, nº 76552
+'
+
+
+Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'Some Constants
+        Dim lblwidth As Short = 75
+        Dim lblheight As Short = 20
+
+        Me.Width = 4 * lblwidth + 3 * 50
+        Me.Height = 4 * lblheight + 5 * 50
+
+        ' Operador 1 Label
+        lblOper1.Location = New Point(50, 50)
+        lblOper1.Text = "Operador 1"
+        lblOper1.Font = New Font(lblOper1.Font, FontStyle.Bold)
+        lblOper1.TextAlign = ContentAlignment.MiddleCenter
+        lblOper1.Width = lblwidth
+        lblOper1.Height = lblheight
+
+        ' Operador 2 Label
+        lblOper2.Location = New Point(lblOper1.Location.X + lblwidth + 50, lblOper1.Location.Y)
+        lblOper2.Text = "Operador 2"
+        lblOper2.Font = New Font(lblOper1.Font, FontStyle.Bold)
+        lblOper2.TextAlign = ContentAlignment.MiddleCenter
+        lblOper2.Width = lblwidth
+        lblOper2.Height = lblheight
+
+        ' Resultado Label
+        lblRes.Location = New Point(lblOper2.Location.X + lblwidth + 50, lblOper1.Location.Y)
+        lblRes.Text = "Resultado"
+        lblRes.Font = New Font(lblOper1.Font, FontStyle.Bold)
+        lblRes.TextAlign = ContentAlignment.MiddleCenter
+        lblRes.Width = lblwidth
+        lblRes.Height = lblheight
+
+        ' Operador 1 Textbox
+        txtOper1.Location = New Point(lblOper1.Location.X, lblOper1.Location.Y + lblheight)
+        txtOper1.Width = lblwidth
+        txtOper1.Height = lblheight
+
+        ' Operador 2 Textbox
+        txtOper2.Location = New Point(lblOper2.Location.X, lblOper2.Location.Y + lblheight)
+        txtOper2.Width = lblwidth
+        txtOper2.Height = lblheight
+
+        ' Resultado Textbox
+        txtRes.Location = New Point(lblRes.Location.X, lblRes.Location.Y + lblheight)
+        txtRes.Width = lblwidth
+        txtRes.Height = lblheight
+
+        ' Soma btn
+        btnSum.Location = New Point(txtOper1.Location.X, txtOper1.Location.Y + lblheight + 50)
+        btnSum.Width = lblwidth
+        btnSum.Height = 2 * lblheight
+        btnSum.Font = New Font(btnSum.Font.ToString, 20, FontStyle.Bold)
+        btnSum.Text = "+"
+
+
+        ' Sub btn
+        btnSub.Location = New Point(txtOper2.Location.X, txtOper2.Location.Y + lblheight + 50)
+        btnSub.Width = lblwidth
+        btnSub.Height = 2 * lblheight
+        btnSub.Font = New Font(btnSum.Font.ToString, 20, FontStyle.Bold)
+        btnSub.Text = "-"
+
+        ' Times btn
+        btnTimes.Location = New Point(btnSum.Location.X, btnSum.Location.Y + lblheight + 50)
+        btnTimes.Width = lblwidth
+        btnTimes.Height = 2 * lblheight
+        btnTimes.Font = New Font(btnSum.Font.ToString, 20, FontStyle.Bold)
+        btnTimes.Text = "*"
+
+        ' Div btn
+        btnDiv.Location = New Point(btnSub.Location.X, btnSub.Location.Y + lblheight + 50)
+        btnDiv.Width = lblwidth
+        btnDiv.Height = 2 * lblheight
+        btnDiv.Font = New Font(btnSum.Font.ToString, 20, FontStyle.Bold)
+        btnDiv.Text = "/"
+
+        ' Sair btn
+        btnExit.Location = New Point(txtRes.Location.X, txtRes.Location.Y + lblheight + 50)
+        btnExit.Width = lblwidth
+        btnExit.Height = lblheight * 2 + 75
+        btnExit.Font = New Font(btnSum.Font.ToString, 16, FontStyle.Bold)
+        btnExit.Text = "Sair"
+
+
+
+    End Sub
+
+    Private Sub BtnSum_Click(sender As Object, e As EventArgs) Handles btnSum.Click
+        Try
+            txtRes.Text = Mid(CStr(CDbl(txtOper1.Text) + CDbl(txtOper2.Text)), 1, 5)
+        Catch ex As Exception
+            MsgBox("Sorry, Error While Computing")
+        End Try
+    End Sub
+
+    Private Sub BtnSub_Click(sender As Object, e As EventArgs) Handles btnSub.Click
+        Try
+            txtRes.Text = Mid(CStr(CDbl(txtOper1.Text) - CDbl(txtOper2.Text)), 1, 5)
+        Catch ex As Exception
+            MsgBox("Sorry, Error While Computing")
+        End Try
+    End Sub
+
+    Private Sub BtnTimes_Click(sender As Object, e As EventArgs) Handles btnTimes.Click
+        Try
+            txtRes.Text = Mid(CStr(CDbl(txtOper1.Text) * CDbl(txtOper2.Text)), 1, 5)
+        Catch ex As Exception
+            MsgBox("Sorry, Error While Computing")
+        End Try
+    End Sub
+
+    Private Sub BtnDiv_Click(sender As Object, e As EventArgs) Handles btnDiv.Click
+        Try
+            txtRes.Text = Mid(CStr(CDbl(txtOper1.Text) / CDbl(txtOper2.Text)), 1, 5)
+        Catch ex As Exception
+            MsgBox("Sorry, Error While Computing")
+        End Try
+
+    End Sub
+
+    Private Sub BtnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+End Class
